3e3cd97f4a4e16988a3d19c1fbb4ea3001644ae0
eslamabosalem <eslamabosalem22@gmail.com>
2025-05-25 07:57:09 +0300

    Done

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ import ContactUs from "./Component/Pages/CountactUs";
 export default function Home() {
   return (
     <>
-  <div className=" mx-1">
+  <div className=" mx-1 ">
 <HomeHero/>
 <About/>
 
