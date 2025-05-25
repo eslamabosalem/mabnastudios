e5c3399c5c38598dced4f09b2480b17ad3b4807b
eslamabosalem <eslamabosalem22@gmail.com>
2025-05-25 05:41:13 +0300

    donoe

--- src/app/Component/Slider.jsx
+++ src/app/Component/Slider.jsx
@@ -167,7 +167,7 @@ export default function Slider() {
       <h1 className="text-4xl font-bold text-center mb-8 text-gray-900 dark:text-white">
         OUR SERVIES
       </h1>
-      
+      {/*  */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8  mx-auto">
         {sliderData.map((section, idx) => (
           <div key={idx} className=" dark:bg-gray-800 rounded-xl  p-4 dark:text-white ">
